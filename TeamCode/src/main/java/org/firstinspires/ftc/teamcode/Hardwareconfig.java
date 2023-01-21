779ef11c202686b3f47708de17191ff3cf3b97d3
Rechin420 <123243671+Rechin420@users.noreply.github.com>
2023-01-21 14:50:26 +0200

    ceva imp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardwareconfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardwareconfig.java
@@ -5,18 +5,32 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.SampleRevBlinkinLedDriver;
+
+import java.sql.SQLRecoverableException;
+
 public class Hardwareconfig {
 
-    public DcMotor stanga;
-    public DcMotor dreapta;
-    public Servo servo_std;
+  public DcMotor rata;
+  public DcMotor intake;
+  public Servo servoIntake;
+  public Servo brat;
+  public Servo cuvaL;
+  public Servo cuvaR;
+  public Servo pins;
+  public Servo crapperClaw;
+  public Servo capper;
 
-    public Hardwareconfig(HardwareMap hw) {
-        stanga=hw.get(DcMotor.class,"stanga");
-        dreapta=hw.get(DcMotor.class,"dreapta");
-        servo_std=hw.get(Servo.class,"servo_std");
 
-        stanga.setDirection(DcMotorSimple.Direction.REVERSE);
-        stanga.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    }
+  public Hardwareconfig(HardwareMap hw){
+      rata=hw.get(DcMotor.class,"rata");
+      intake=hw.get(DcMotor.class,"Intake");
+      servoIntake=hw.get(Servo.class,"servoIntake");
+      brat=hw.get(Servo.class, "brat");
+      cuvaL=hw.get(Servo.class, "cuvaL");
+      cuvaR=hw.get(Servo.class, "cuvaR");
+      pins=hw.get(Servo.class, "pins");
+      crapperClaw=hw.get(Servo.class, "crapperClaw");
+      capper=hw.get(Servo.class, "capper");
+  }
 }
