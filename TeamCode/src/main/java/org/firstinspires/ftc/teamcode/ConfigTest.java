fa18c406f6d429597d86a44e460cc5713f6288b4
Aluching <123243725+Aluching@users.noreply.github.com>
2023-01-21 15:04:04 +0000

    Facut configuratie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConfigTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConfigTest.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
 public class ConfigTest extends OpMode {
         public Hardwareconfig hw;
 
@@ -13,7 +15,7 @@ public class ConfigTest extends OpMode {
     public void loop() {
         double RT=gamepad1.right_trigger;
         float LT=gamepad1.left_trigger;
-        hw.rata.setPower(RT);
+        hw.rate.setPower(RT);
         hw.intake.setPower(LT);
 
         if(gamepad1.dpad_up)
@@ -21,15 +23,16 @@ public class ConfigTest extends OpMode {
         else if (gamepad1.dpad_down) { hw.cuvaR.setPosition(0.6);}
 
         if (gamepad1.dpad_left)
-            hw.capper.setPosition(0.5);
-        else if (gamepad1.dpad_right) { hw.capper.setPosition(0.6);}
+            hw.capperBrat.setPosition(0.5);
+        else if (gamepad1.dpad_right) { hw.capperBrat.setPosition(0.6);}
 
         if (gamepad1.start)
-            hw.pins.setPosition(0.5);
+            hw.impins.setPosition(0.35);
 
         if (gamepad1.x)
-            hw.brat.setPosition(0.5);
-        else if (gamepad1.y) {hw.crapperClaw.setPosition(0.5);}
-        //
+            hw.capperClaw.setPosition(0.5);
+        else if (gamepad1.y) {
+            hw.capperClaw.setPosition(0.6);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardwareconfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardwareconfig.java
@@ -1,36 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.SampleRevBlinkinLedDriver;
-
-import java.sql.SQLRecoverableException;
-
 public class Hardwareconfig {
 
-  public DcMotor rata;
+  public DcMotor rate;
   public DcMotor intake;
   public Servo servoIntake;
-  public Servo brat;
   public Servo cuvaL;
   public Servo cuvaR;
-  public Servo pins;
-  public Servo crapperClaw;
-  public Servo capper;
+  public Servo impins;
+  public Servo capperClaw;
+  public Servo capperBrat;
 
 
   public Hardwareconfig(HardwareMap hw) {
-      rata = hw.get(DcMotor.class, "rata");
-      intake = hw.get(DcMotor.class, "Intake");
+      rate = hw.get(DcMotor.class, "rate");
+      intake = hw.get(DcMotor.class, "intake");
       servoIntake = hw.get(Servo.class, "servoIntake");
-      brat = hw.get(Servo.class, "brat");
       cuvaL = hw.get(Servo.class, "cuvaL");
       cuvaR = hw.get(Servo.class, "cuvaR");
-      pins = hw.get(Servo.class, "pins");
-      crapperClaw = hw.get(Servo.class, "crapperClaw");
-      capper = hw.get(Servo.class, "capper");
+      impins = hw.get(Servo.class, "impins");
+      capperClaw = hw.get(Servo.class, "capperClaw");
+      capperBrat = hw.get(Servo.class, "capperBrat");
   }
 }
